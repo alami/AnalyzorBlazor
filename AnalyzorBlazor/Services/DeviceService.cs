139a94f5cb550f48710e8806f507b47eea3de156
tas_admin <tas_admin@hq.int.unesco.org>
2024-04-03 13:12:46 +0500

    add tester/device/delete

--- AnalyzorBlazor/Services/DeviceService.cs
+++ AnalyzorBlazor/Services/DeviceService.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using AnalyzerBlasor.Data;
 using AnalyzorBlazor.Models;
+using Azure;
+using AnalyzerBlasor.Services.Base;
 
 
 namespace AnalyzorBlazor.Services
@@ -8,16 +10,59 @@ namespace AnalyzorBlazor.Services
     public class DeviceService : IDeviceService
     {
         private readonly ApplicationDbContext _db;
-
         public DeviceService(ApplicationDbContext db)
         {
             _db = db;
         }
-        public List<Device> Index()
+        public async Task<List<Device>> Get()
         {
             List<Device> objList = _db.Device.ToList();
             return objList;
+        }
+        public async Task<Responses<int>> Delete(int id)
+        {
+            Responses<int> responses = new();
+            try
+            {
+                var obj = await _db.Device.FindAsync(id);
+                if (obj == null)
+                {
+                    responses.Success = false;                    
+                    responses.Message = $"Device/Delete/{id} Not found";                    
+                }
+                else
+                {
+                    _db.Device.Remove(obj);
+                    //_db.SaveChanges();
+                    responses.Success = true;
+                    responses.Message = $"Device/Delete/{id} was deleted";
+                }                
+            } catch (Exception ex)
+            {
+                responses.Success = false;
+                responses.Message = $"Device/Delete/{id} Exception";
+            }
+            return responses;
+        }
+        public Task<Responses<int>> Create(Device device)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<Responses<int>> Edit(int id, Device device)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<Responses<Device>> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Responses<Device>> GetForUpdate(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- AnalyzorBlazor/Services/IDeviceService.cs
+++ AnalyzorBlazor/Services/IDeviceService.cs
@@ -1,10 +1,17 @@
-﻿using AnalyzorBlazor.Models;
+﻿using AnalyzerBlasor.Services.Base;
+using AnalyzorBlazor.Models;
+using Azure;
 
 namespace AnalyzorBlazor.Services
 {
     public interface IDeviceService
     {
-        public List<Device> Index();
+        Task<List<Device>> Get();
+        Task<Responses<Device>> Get(int id);
+        Task<Responses<Device>> GetForUpdate(int id);
+        Task<Responses<int>> Create(Device book);
+        Task<Responses<int>> Edit(int id, Device book);
+        Task<Responses<int>> Delete(int id);
 
     }
 }
