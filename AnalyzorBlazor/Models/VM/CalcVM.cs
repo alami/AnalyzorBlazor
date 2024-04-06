eb7cc9ce0ad9122a7d4660b2738f4d127fe7c2af
tas_admin <tas_admin@hq.int.unesco.org>
2024-04-06 16:25:28 +0500

    add analyzer calc v1

---
+++ AnalyzorBlazor/Models/VM/CalcVM.cs
@@ -0,0 +1,10 @@
+﻿namespace AnalyzorBlazor.Models.VM
+{
+    public class CalcVM
+    {
+        public Device Device { get; set; }
+        public List<DeviceComponent>? DevCompAssList { get; set; }
+        public List<DeviceComponent>? DevCompPartsList { get; set; }
+        public List<Component>? CompList { get; set; }
+    }
+}
